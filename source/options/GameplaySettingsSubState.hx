33ddb599fb4e2c3ca5dec49de5b306e84f3429f4
primwater <98289843+primwater@users.noreply.github.com>
2022-01-31 16:36:02 -0600

    Update GameplaySettingsSubState.hx

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -1,8 +1,5 @@
 package options;
 
-#if desktop
-import Discord.DiscordClient;
-#end
 import flash.text.TextField;
 import flixel.FlxG;
 import flixel.FlxSprite;
@@ -17,7 +14,6 @@ import flash.text.TextField;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxSave;
-import haxe.Json;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
@@ -27,115 +23,130 @@ import Controls;
 
 using StringTools;
 
-class GameplaySettingsSubState extends BaseOptionsMenu
+class Option
 {
-	public function new()
+	private var child:Alphabet;
+	public var text(get, set):String;
+	public var onChange:Void->Void = null; //Pressed enter (on Bool type options) or pressed/held left/right (on other types)
+
+	public var type(get, default):String = 'bool'; //bool, int (or integer), float (or fl), percent, string (or str)
+	// Bool will use checkboxes
+	// Everything else will use a text
+
+	public var showBoyfriend:Bool = false;
+	public var scrollSpeed:Float = 50; //Only works on int/float, defines how fast it scrolls per second while holding left/right
+
+	private var variable:String = null; //Variable from ClientPrefs.hx
+	public var defaultValue:Dynamic = null;
+
+	public var curOption:Int = 0; //Don't change this
+	public var options:Array<String> = null; //Only used in string type
+	public var changeValue:Dynamic = 1; //Only used in int/float/percent type, how much is changed when you PRESS
+	public var minValue:Dynamic = null; //Only used in int/float/percent type
+	public var maxValue:Dynamic = null; //Only used in int/float/percent type
+	public var decimals:Int = 1; //Only used in float/percent type
+
+	public var displayFormat:String = '%v'; //How String/Float/Percent/Int values are shown, %v = Current value, %d = Default value
+	public var description:String = '';
+	public var name:String = 'Unknown';
+
+	public function new(name:String, description:String = '', variable:String, type:String = 'bool', defaultValue:Dynamic = 'null variable value', ?options:Array<String> = null)
 	{
-		title = 'Gameplay Settings';
-		rpcTitle = 'Gameplay Settings Menu'; //for Discord Rich Presence
-		
-		var option:Option = new Option('Controller Mode',
-			'Check this if you want to play with\na controller instead of using your Keyboard.',
-			'controllerMode',
-			'bool',
-			false);
-		addOption(option);
+		this.name = name;
+		this.description = description;
+		this.variable = variable;
+		this.type = type;
+		this.defaultValue = defaultValue;
+		this.options = options;
 
-		//I'd suggest using "Downscroll" as an example for making your own option since it is the simplest here
-		var option:Option = new Option('Downscroll', //Name
-			'If checked, notes go Down instead of Up, simple enough.', //Description
-			'downScroll', //Save data variable name
-			'bool', //Variable type
-			false); //Default value
-		addOption(option);
+		if(defaultValue == 'null variable value')
+		{
+			switch(type)
+			{
+				case 'bool':
+					defaultValue = false;
+				case 'int' | 'float':
+					defaultValue = 0;
+				case 'percent':
+					defaultValue = 1;
+				case 'string':
+					defaultValue = '';
+					if(options.length > 0) {
+						defaultValue = options[0];
+					}
+			}
+		}
 
-		var option:Option = new Option('Middlescroll',
-			'If checked, your notes get centered.',
-			'middleScroll',
-			'bool',
-			false);
-		addOption(option);
+		if(getValue() == null) {
+			setValue(defaultValue);
+		}
 
-		var option:Option = new Option('Ghost Tapping',
-			"If checked, you won't get misses from pressing keys\nwhile there are no notes able to be hit.",
-			'ghostTapping',
-			'bool',
-			true);
-		addOption(option);
+		switch(type)
+		{
+			case 'string':
+				var num:Int = options.indexOf(getValue());
+				if(num > -1) {
+					curOption = num;
+				}
+	
+			case 'percent':
+				displayFormat = '%v%';
+				changeValue = 0.01;
+				minValue = 0;
+				maxValue = 1;
+				scrollSpeed = 0.5;
+				decimals = 2;
+		}
+	}
 
-		var option:Option = new Option('Disable Reset Button',
-			"If checked, pressing Reset won't do anything.",
-			'noReset',
-			'bool',
-			false);
-		addOption(option);
+	public function change()
+	{
+		//nothing lol
+		if(onChange != null) {
+			onChange();
+		}
+	}
 
-		/*var option:Option = new Option('Note Delay',
-			'Changes how late a note is spawned.\nUseful for preventing audio lag from wireless earphones.',
-			'noteOffset',
-			'int',
-			0);
-		option.displayFormat = '%vms';
-		option.scrollSpeed = 100;
-		option.minValue = 0;
-		option.maxValue = 500;
-		addOption(option);*/
+	public function getValue():Dynamic
+	{
+		return Reflect.getProperty(ClientPrefs, variable);
+	}
+	public function setValue(value:Dynamic)
+	{
+		Reflect.setProperty(ClientPrefs, variable, value);
+	}
 
-		var option:Option = new Option('Rating Offset',
-			'Changes how late/early you have to hit for a "Sick!"\nHigher values mean you have to hit later.',
-			'ratingOffset',
-			'int',
-			0);
-		option.displayFormat = '%vms';
-		option.scrollSpeed = 20;
-		option.minValue = -30;
-		option.maxValue = 30;
-		addOption(option);
+	public function setChild(child:Alphabet)
+	{
+		this.child = child;
+	}
 
-		var option:Option = new Option('Sick! Hit Window',
-			'Changes the amount of time you have\nfor hitting a "Sick!" in miliseconds.',
-			'sickWindow',
-			'int',
-			45);
-		option.displayFormat = '%vms';
-		option.scrollSpeed = 15;
-		option.minValue = 15;
-		option.maxValue = 45;
-		addOption(option);
+	private function get_text()
+	{
+		if(child != null) {
+			return child.text;
+		}
+		return null;
+	}
+	private function set_text(newValue:String = '')
+	{
+		if(child != null) {
+			child.changeText(newValue);
+		}
+		return null;
+	}
 
-		var option:Option = new Option('Good Hit Window',
-			'Changes the amount of time you have\nfor hitting a "Good" in miliseconds.',
-			'goodWindow',
-			'int',
-			90);
-		option.displayFormat = '%vms';
-		option.scrollSpeed = 30;
-		option.minValue = 15;
-		option.maxValue = 90;
-		addOption(option);
-
-		var option:Option = new Option('Bad Hit Window',
-			'Changes the amount of time you have\nfor hitting a "Bad" in miliseconds.',
-			'badWindow',
-			'int',
-			135);
-		option.displayFormat = '%vms';
-		option.scrollSpeed = 60;
-		option.minValue = 15;
-		option.maxValue = 135;
-		addOption(option);
-
-		var option:Option = new Option('Safe Frames',
-			'Changes how many frames you have for\nhitting a note earlier or late.',
-			'safeFrames',
-			'float',
-			10);
-		option.scrollSpeed = 5;
-		option.minValue = 2;
-		option.maxValue = 10;
-		option.changeValue = 0.1;
-		addOption(option);
-
-		super();
+	private function get_type()
+	{
+		var newValue:String = 'bool';
+		switch(type.toLowerCase().trim())
+		{
+			case 'int' | 'float' | 'percent' | 'string': newValue = type;
+			case 'integer': newValue = 'int';
+			case 'str': newValue = 'string';
+			case 'fl': newValue = 'float';
+		}
+		type = newValue;
+		return type;
 	}
 }
